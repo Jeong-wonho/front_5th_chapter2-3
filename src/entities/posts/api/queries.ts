05c0d270179df4b0ecd53ae815e47f5f3444356a
Jeong-wonho <kallzzang@naver.com>
2025-05-02 02:44:55 +0900

    배포 진행중

--- src/entities/posts/api/queries.ts
+++ src/entities/posts/api/queries.ts
@@ -1,5 +1,4 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { PostResponse } from '../models'
 import { addPostData, deletePostData, getPosts, updatePostData } from './posts'
 
 // Query Keys
