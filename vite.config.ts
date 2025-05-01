7a6384c7737c32d9a53b5e73908edf915ede7577
Jeong-wonho <kallzzang@naver.com>
2025-05-02 03:24:13 +0900

    배포

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,41 @@
-import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
-import { loadEnv } from "vite"
 import { resolve } from "path"
-const mode = process.env.NODE_ENV || "development"
-const env = loadEnv(mode, process.cwd(), "")
-
+import { defineConfig, Plugin } from "vite"
+// https://vite.dev/config/
 export default defineConfig({
-  base: env.VITE_BASE_PATH,
-		plugins: [react()],
-		build: {
-			rollupOptions: {
-				input: {
-					main: resolve(__dirname, "index.html"),
-				},
-			},
-		},
+  plugins: [react(), apiReplace()],
+  base: "/front_5th_chapter2-3/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+    outDir: "dist",
+  },
+
+  server: {
+    proxy: {
+      "/api": {
+        // target: 'https://jsonplaceholder.typicode.com',
+        target: "https://dummyjson.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
+
+function apiReplace(): Plugin {
+  return {
+    name: "api-replace",
+    apply: "build",
+    transform(code, id) {
+      if (!id.match(/\.(ts|js|tsx|jsx)$/)) return
+      return code.replace(
+        /(['"`])\/api([^"'`\\]*)\1/g,
+        (_, quote, apiPath) => `${quote}https://dummyjson.com${apiPath}${quote}`,
+      )
+    },
+  }
+}
